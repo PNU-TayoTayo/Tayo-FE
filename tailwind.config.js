507a3a1fbb3d21aa69102c01715f82f60e0b4142
jihyeon <wlgus7464@gmail.com>
2023-07-09 01:10:05 +0900

    tailwindCSS, typescript, 상대경로 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+const px0_2000 = { ...Array.from(Array(2001)).map((_, i) => `${i}px`) };
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +12,39 @@ module.exports = {
   ],
   theme: {
     extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_2000,
+      minHeight: px0_2000,
+      maxWidth: px0_2000,
+      maxHeight: px0_2000,
+      spacing: px0_200,
+      borderRadius: px0_100,
+      width: px0_2000,
+      height: px0_2000,
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    fontFamily: {
+      sans: ['Noto Sans KR', 'sans-serif'],
+      GMarket : ['GmarketSans','sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      mainGreen: '#2EB573',
+      subGreen: '#6BC79A',
+      pointYellow: '#FFA710',
+      lightGrey: '#dedede',
+      title: '#394159',
+    },
+    screens: {
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
     },
   },
   plugins: [],
