84566c8b94262eb7f9721203172a1f8fa3d988bf
jihyeon <wlgus7464@gmail.com>
2023-08-16 00:25:54 +0900

    feat: sign-in wip

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, {useRef, useState} from 'react';
-import Car from '@image/layout/sports-car.svg'
+import React, {useEffect, useRef, useState} from 'react';
+import Car from '@image/layout/car-green.svg'
 import InputWrapper from "@components/common/InputWrapper";
 import {useForm} from "react-hook-form";
 import Image from "next/image";
@@ -13,17 +13,31 @@ const SignIn = () => {
     const {register, handleSubmit, watch, reset, setValue, formState: {errors}, clearErrors} = useForm<LoginForm>()
     const inputRefs = useRef({id: null, password: null})
     const [isInputFocused, setIsInputFocused] = useState({id: false, password: false})
-
+    const [form, setForm] = useState({
+        id: watch('id'),
+        password: watch('password'),
+    });
     const onClickInput = (type: string) => {
         setIsInputFocused({...isInputFocused, [type]: true})
         inputRefs.current[type].focus();
     }
 
+    useEffect(() => {
+        if (watch('id')) {
+            clearErrors('id')
+        }
+        if (watch('password')) {
+            clearErrors('password')
+        }
+    }, [form.id, form.password])
+
     return (
-        <div className={`w-full h-full bg-subGreen`}>
-            <div className={`flex w-690 h-530 justify-center items-center`}>
-                <form className="w-396 flex flex-col items-center gap-y-68">
+        <div className={`flex w-screen h-screen justify-center items-center bg-subGreen`}>
+            <div className={`flex w-690 h-530 justify-center items-center bg-white bg-opacity-50 rounded-20 m-auto`}>
+                <form className="w-396 flex flex-col items-center">
                     <Image src={Car} alt={'car'}/>
+                    <p>스마트 시티 공유 모빌리티 서비스, <b>타요타요</b></p>
+                    <p>진정한 공유로 연결되는 스마트 시티를 함께 만들어가요</p>
                     <div className="w-full mb-50">
                         <InputWrapper
                             register={register('id', {required: true})}
@@ -39,6 +53,7 @@ const SignIn = () => {
                             onClick={() => onClickInput('password')}
                             setValue={setValue}
                             label="비밀번호" type="passWord"/>
+                        {/*서버 에러 메세지 받아와서 처리*/}
                         {/*{*/}
                         {/*    error && (*/}
                         {/*        <div className="mt-10 absolute">*/}
