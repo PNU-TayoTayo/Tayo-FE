9edb65cd183b570db624bf742e408b1d5bc157c7
jihyeon <wlgus7464@gmail.com>
2023-09-16 11:56:10 +0900

    feat: 로그인

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -5,16 +5,15 @@ import InputWrapper from "@components/common/InputWrapper";
 import {useForm} from "react-hook-form";
 import Image from "next/image";
 import TayoButton from "@components/common/TayoButton";
-interface LoginForm {
-    id: string;
-    password: string;
-}
+import WhiteBox from "@components/common/WhiteBox";
+import {UserLoginInfo} from "@type/UserSignUpInfo";
+
 const SignIn = () => {
-    const {register, handleSubmit, watch, reset, setValue, formState: {errors}, clearErrors} = useForm<LoginForm>()
+    const {register, handleSubmit, watch, reset, setValue, formState: {errors}, clearErrors} = useForm<UserLoginInfo>()
     const inputRefs = useRef({id: null, password: null})
     const [isInputFocused, setIsInputFocused] = useState({id: false, password: false})
     const [form, setForm] = useState({
-        id: watch('id'),
+        id: watch('email'),
         password: watch('password'),
     });
     const onClickInput = (type: string) => {
@@ -23,8 +22,8 @@ const SignIn = () => {
     }
 
     useEffect(() => {
-        if (watch('id')) {
-            clearErrors('id')
+        if (watch('email')) {
+            clearErrors('email')
         }
         if (watch('password')) {
             clearErrors('password')
@@ -34,34 +33,59 @@ const SignIn = () => {
     return (
         <div className={`flex w-screen h-screen justify-center items-center bg-subGreen`}>
             <div className={`flex w-690 h-530 justify-center items-center bg-white bg-opacity-50 rounded-20 m-auto`}>
-                <form className="w-396 flex flex-col items-center">
-                    <Image src={Car} alt={'car'}/>
-                    <p>스마트 시티 공유 모빌리티 서비스, <b>타요타요</b></p>
-                    <p>진정한 공유로 연결되는 스마트 시티를 함께 만들어가요</p>
-                    <div className="w-full mb-50">
-                        <InputWrapper
-                            register={register('id', {required: true})}
-                            isError={errors.id && true} isInputOpen={isInputFocused.id}
-                            ref={(ref) => (inputRefs.current.id = ref)}
-                            onClick={() => onClickInput('id')}
-                            setValue={setValue}
-                            label="아이디" className="mb-48" type="text"/>
-                        <InputWrapper
-                            register={register('password', {required: true})}
-                            isError={errors.password && true} isInputOpen={isInputFocused.password}
-                            ref={(ref) => (inputRefs.current.password = ref)}
-                            onClick={() => onClickInput('password')}
-                            setValue={setValue}
-                            label="비밀번호" type="passWord"/>
-                        {/*서버 에러 메세지 받아와서 처리*/}
-                        {/*{*/}
-                        {/*    error && (*/}
-                        {/*        <div className="mt-10 absolute">*/}
-                        {/*            <Image src={warning} className="inline mr-4" alt=""/>*/}
-                        {/*            <p className="inline text-14 text-[#e10000]">{error.toString()}</p>*/}
-                        {/*        </div>*/}
-                        {/*    )*/}
-                        {/*}*/}
+                <form className="w-500 h-full flex flex-col items-center justify-between py-100">
+                    <div className={`flex flex-col gap-16 text-center`}>
+                        <Image src={Car} alt={'car'} width={48} height={48} className={`m-auto`}/>
+                        <div>
+                            <p className={`text-16`}>스마트 시티 공유 모빌리티 서비스, <b>타요타요</b></p>
+                            <p className={`text-16`}>진정한 공유로 연결되는 스마트 시티를 함께 만들어가요</p>
+                        </div>
+                    </div>
+                    <div className={`flex flex-col gap-16`}>
+                        <div className={`flex justify-between`}>
+                            <label className={`text-14 text-black`}>이메일</label>
+                            <div className={`flex flex-col`}>
+                                <WhiteBox className={`w-390`}>
+                                    <input className={`w-full outline-none`}
+                                           type="text"
+                                           {...register('email', {
+                                               required: '이메일은 필수 입력 항목입니다.',
+                                               pattern: {
+                                                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                                   message: '올바른 이메일 형식이 아닙니다.',
+                                               },
+                                           })}
+                                    />
+                                </WhiteBox>
+                                <span>{errors.email && errors.email.message}</span>
+                            </div>
+                        </div>
+                        <div className={`flex gap-30`}>
+                            <label className={`text-14 text-black`}>비밀번호</label>
+                            <div className={`flex flex-col`}>
+                                <WhiteBox className={`w-390`}>
+                                    <input className={`w-full outline-none`}
+                                           type="password"
+                                           {...register('password', {
+                                               required: '비밀번호는 필수 입력 항목입니다.',
+                                               minLength: {
+                                                   value: 8,
+                                                   message: '비밀번호는 최소 8글자 이상이어야 합니다.',
+                                               },
+                                               maxLength: {
+                                                   value: 20,
+                                                   message: '비밀번호는 최대 20글자까지 허용됩니다.',
+                                               },
+                                               pattern: {
+                                                   value: /^(?=.*\d)(?=.*[a-zA-Z]).*$/,
+                                                   message: '비밀번호는 영문과 숫자의 조합이어야 합니다.',
+                                               },
+                                           })}
+                                    />
+                                </WhiteBox>
+                                <span>{errors.password && errors.password.message}</span>
+                            </div>
+                        </div>
                     </div>
                     <TayoButton maxWidth={'max-w-477'} height={'h-56'} rounded={'rounded-10'} onClick={()=>{}}>로그인</TayoButton>
                 </form>
--- src/components/common/TayoButton.tsx
+++ src/components/common/TayoButton.tsx
@@ -4,7 +4,7 @@ import Button from "@components/common/Button";
 const TayoButton = ({onClick, children, maxWidth='', height='h-67', rounded='rounded-10', leading='', disabled = false, className = '', ...props}) => {
     return (
         <Button onClick={onClick} disabled={disabled}
-                className={`${maxWidth} ${height} ${rounded} ${leading} w-full text-white bg-mainGreen text-center ${className}`}
+                className={`${maxWidth} ${height} ${rounded} ${leading} w-full text-white ${disabled?'bg-[#A3A3A3] bg-opacity-80':'bg-mainGreen'} text-center ${className}`}
                 {...props}>
             {children}
         </Button>
