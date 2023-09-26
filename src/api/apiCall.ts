714392d7709d282086ef7ac7dfbc37cc6ed38299
jihyeon <wlgus7464@gmail.com>
2023-09-26 09:33:16 +0900

    refactor: apiCall 수정

--- src/api/apiCall.ts
+++ src/api/apiCall.ts
@@ -4,11 +4,11 @@ export default async function apiCall(fn) {
         return response.data;
     }catch (error) {
         if(error.response) {
-            if(error.response.status === 401) {
+            if(error.response.data.status === 401) {
                 location.href = '/login';
                 return;
             } else {
-                const errorMessage = error.response.data?.responseData?.message || error.response.data?.error?.message || '오류가 발생했습니다.';
+                const errorMessage = error.response.message || '오류가 발생했습니다.';
                 alert(errorMessage);
                 return;
             }
