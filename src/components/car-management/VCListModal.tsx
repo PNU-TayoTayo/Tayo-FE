51b61a5ce664110a397bb1725bc538a227747b20
jihyeon <wlgus7464@gmail.com>
2023-10-11 00:10:49 +0900

    feat: vp 등록 wip

--- src/components/car-management/VCListModal.tsx
+++ src/components/car-management/VCListModal.tsx
@@ -3,7 +3,7 @@ import Unchecked from '@image/car-management/Check_ring_light.svg';
 import Checked from '@image/car-management/Check_fill.svg';
 import Image from "next/image";
 import apiCall from "@api/apiCall";
-import {getVCList} from "@api/carManagementApi";
+import {getVCList, registerCar} from "@api/carManagementApi";
 import GrayBox from "@components/common/GrayBox";
 
 const VCListModal = ({open, onClose, }) => {
@@ -27,6 +27,7 @@ const VCListModal = ({open, onClose, }) => {
         drivingRecord: "130",
     }]
     const [walletPassword, setWalletPassword] = useState<string>('');
+    const [isWalletPasswordEntered, setIsWalletPasswordEntered] = useState<boolean>(false); // 지갑 비밀번호 맞는지 여부
     const [vcList, setVcList] = useState<CarVC[]>([]);
     const [selectedVC, setSelectedVC] = useState(null); // 선택된 VC 아이템 상태
     const [myVcData, setMyVcData] = useState<CarVC>();
@@ -40,7 +41,7 @@ const VCListModal = ({open, onClose, }) => {
         }
     }
     const handleWalletPassword = () => {
-        VCList({walletPassword})
+        VCList({walletPassword}).then(()=>{setIsWalletPasswordEntered(true)})
     }
     const handleVCItemClick = (index) => {
         setSelectedVC(index)
@@ -49,9 +50,12 @@ const VCListModal = ({open, onClose, }) => {
         setMyVcData(data);
         setCurrentStep(2);
     }
-    const handleClickRegister = ()  => {
-        //TODO: api call 차량 등록
+    const handleClickCancelButton = () => {
+        setCurrentStep(1);
+        setVcList([]);
+        onClose();
     }
+
     return (
         <div className={`absolute top-0 left-0 flex w-full h-full bg-black bg-opacity-50 items-center ${!open&&'hidden'} z-10`}>
             <div className={`relative flex flex-col bg-white w-700 h-700 rounded-8 m-auto p-36`}>
@@ -69,13 +73,13 @@ const VCListModal = ({open, onClose, }) => {
                             VC 확인
                         </button>
                     </div>
-                    <div className={`flex flex-col gap-24 overflow-auto scrollbar-hide mt-24 mb-80`}>
+                    <div className={`flex flex-col gap-24 h-390 overflow-auto scrollbar-hide mt-24 mb-80`}>
                         {vcList && vcList.map((item, index) => {
                             return (
-                                <div key={index} className={`flex gap-16 cursor-pointer`}>
+                                <div key={index} className={`flex gap-16 cursor-pointer`}
+                                     onClick={() => handleVCItemClick(index)}>
                                     <Image src={selectedVC === index ? Checked : Unchecked} // 선택 여부에 따라 이미지 변경
-                                           alt={'check'} width={73} height={73}
-                                           onClick={() => handleVCItemClick(index)}/>
+                                           alt={'check'} width={73} height={73}/>
                                     <VCItem info={item}/>
                                 </div>
                             );
@@ -83,10 +87,11 @@ const VCListModal = ({open, onClose, }) => {
                     </div>
                     <div className={`absolute bottom-36 left-110 flex justify-center text-center gap-24`}>
                         <button className={`w-227 h-62 bg-lightGrey text-white text-20 font-bold rounded-4`}
-                                onClick={onClose}>
+                                onClick={handleClickCancelButton}>
                             취소
                         </button>
                         <button className={`w-227 h-62 bg-subGreen text-white text-20 font-bold rounded-4`}
+                                disabled={!isWalletPasswordEntered}
                                 onClick={() => {
                                     handleVCSelectClick(vcList[selectedVC])
                                 }}>
@@ -94,7 +99,7 @@ const VCListModal = ({open, onClose, }) => {
                         </button>
                     </div>
                 </div>}
-                {currentStep===2&&<RegisterVP vcData={myVcData} onClose={onClose}/>}
+                {currentStep===2&&<RegisterVP vcData={myVcData} walletPassword={walletPassword} handleClickCancelButton={handleClickCancelButton}/>}
             </div>
         </div>
     );
@@ -133,11 +138,25 @@ const VCItem = ({info}) => {
     )
 }
 
-const RegisterVP = ({vcData, onClose}) => {
+const RegisterVP = ({vcData, walletPassword, handleClickCancelButton}) => {
     const [location, setLocation] = useState(''); // 위치 상태
     const [rentalFee, setRentalFee] = useState(''); // 대여료 상태
     const [availabilityDate, setAvailabilityDate] = useState(''); // 공유 가능 일자 상태
 
+    const postCar = async () => {
+        const response = await apiCall(registerCar({
+            walletPassword,
+            referentVC: vcData?.referentVC,
+            location : {
+                sharingLocation: location,
+                sharingLocationAddress: location,
+                sharingLatitude: '35.23258237080505',
+                sharingLongitude: '129.0828602625644'
+            },
+            sharingPrice: rentalFee,
+            timeList: [availabilityDate,]
+        }))
+    }
     const handleLocationChange = (e) => {
         setLocation(e.target.value);
     };
@@ -153,6 +172,9 @@ const RegisterVP = ({vcData, onClose}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
     };
+    const handleClickRegister = ()  => {
+        postCar()//.then(()=>{handleClickCancelButton()})
+    }
 
     return (
         <form onSubmit={handleSubmit}>
@@ -190,15 +212,31 @@ const RegisterVP = ({vcData, onClose}) => {
                         className={`text-23 outline-none w-full`}
                     />
                 </div>
+                <div className={`flex justify-between`}>
+                    <label className={`text-23 w-full `}>사용 연료:</label>
+                    <span className={`text-23`}>{vcData?.carFuel}</span>
+                </div>
+                <div className={`flex justify-between`}>
+                    <label className={`text-23 w-full `}>최초 등록:</label>
+                    <span className={`text-23`}>{vcData?.carDeliveryDate}</span>
+                </div>
+                <div className={`flex justify-between`}>
+                    <label className={`text-23 w-full `}>주행 거리:</label>
+                    <span className={`text-23`}>{vcData?.drivingRecord}</span>
+                </div>
+                <div className={`flex justify-between`}>
+                    <label className={`text-23 w-full `}>검사 이력:</label>
+                    <span className={`text-23`}>{vcData?.inspectionRecord}</span>
+                </div>
             </div>
             <div className={`absolute bottom-36 left-110 flex justify-center text-center gap-24`}>
                 <button className={`w-227 h-62 bg-lightGrey text-white text-20 font-bold rounded-4`}
-                        onClick={onClose}>
+                        onClick={handleClickCancelButton}>
                     취소
                 </button>
                 <button className={`w-227 h-62 bg-subGreen text-white text-20 font-bold rounded-4`}
                         onClick={() => {
-                            handleVCSelectClick(vcList[selectedVC])
+                            handleClickRegister()
                         }}>
                     등록
                 </button>
